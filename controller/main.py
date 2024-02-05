a9c865f98796619a6911d158caba660bcc5e4ad8
Murshid <muhsink00000@gmail.com>
2024-02-05 11:23:44 +0530

    changed form coordinator field

--- controller/main.py
+++ controller/main.py
@@ -20,8 +20,8 @@ class GrievanceWebsiteForm(http.Controller):
 
     @http.route(['/grievance_form/submit'], type='http', auth="public", website=True, csrf=False)
     def grievance_form_submit(self, **kw):
-        print(kw, 'ooo')
         file = kw.get('attach_file')
+        print(kw, 'file')
 
         request.env['grievance.form'].sudo().create({
             'name': kw.get('name'),
@@ -34,15 +34,13 @@ class GrievanceWebsiteForm(http.Controller):
             'priority': kw.get('priority'),
             'course_id': kw.get('course_id'),
             'branch_id': kw.get('branch_id'),
-            'coordinator': kw.get('coordinator'),
-            # 'expected_resolution_date': kw.get('expecting_closing'),
             'attach_file': base64.b64encode(file.read()),
             'type_of_issue': kw.get('type_of_issue'),
 
         })
-        type = request.env['grievance.form.type'].sudo().search([('id', '=', kw.get('type_of_issue'))])
-        activity = request.env['grievance.form'].sudo().search([], order='id desc', limit=1)
-        print(activity.name, 'activity')
+        # type = request.env['grievance.form.type'].sudo().search([('id', '=', kw.get('type_of_issue'))])
+        # activity = request.env['grievance.form'].sudo().search([], order='id desc', limit=1)
+        # print(activity.name, 'activity')
 
         # activity.activity_schedule('grievance_form.mail_activity_grievance_form', res_id=activity.id, user_id=type.assigned_to.id,
         #                     note=f'This batch grievance added.')
@@ -53,4 +51,4 @@ class GrievanceWebsiteForm(http.Controller):
         #                                    note=f'This batch grievance added.')
 
         #
-        return request.render("logic_grievance.tmp_grievance_form_success", {})
+        return request.render("logic_grievance.template_grievance_form_success", {})
